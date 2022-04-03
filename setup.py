2f3ef0193e0c40209d0d51a37e565886558e71d5
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-04-03 22:31:00 +0000

    Bump to version 0.5.5

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools  # type: ignore
 
-MAJOR, MINOR, PATCH = 0, 5, 4
+MAJOR, MINOR, PATCH = 0, 5, 5
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 """This project uses semantic versioning.  See https://semver.org/ Before MAJOR = 1, there is no promise for
 backwards compatibility between minor versions.
