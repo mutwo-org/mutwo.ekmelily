dafefa06e18fce110da8642cb867cd491fc4b178
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2022-08-10 22:15:57 +0200

    Upgrade mutwo dependencies

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ setuptools.setup(
     ],
     setup_requires=[],
     install_requires=[
-        "mutwo.ext-core>=0.59.0, <0.60.0",
-        "mutwo.ext-music>=0.14.0, <0.17.0",
+        "mutwo.core>=0.61.2, <1.00.0",
+        "mutwo.music>=0.17.0, <1.0.0",
     ],
     extras_require=extras_require,
     python_requires=">=3.9, <4",
