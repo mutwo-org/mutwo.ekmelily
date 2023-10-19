b6b90e79b4933ab609ca0bcc6df0f78aa2937546
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-10-20 00:59:56 +0200

    Use mutwo-nix, but with local src

--- default.nix
+++ default.nix
@@ -1,31 +1,10 @@
-with import <nixpkgs> {};
-with pkgs.python310Packages;
-
 let
-
-  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/475439a90bb7bf2d429bad93637a22e08f1a4e89.tar.gz";
-  mutwo-music = import (mutwo-music-archive + "/default.nix");
-
+  sourcesTarball = fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/refs/heads/main.tar.gz";
+  mutwo-ekmelily = import (sourcesTarball + "/mutwo.ekmelily/default.nix") {};
+  mutwo-ekmelily-local = mutwo-ekmelily.overrideAttrs (
+    finalAttrs: previousAttrs: {
+       src = ./.;
+    }
+  );
 in
-
-  buildPythonPackage rec {
-    name = "mutwo.ekmelily";
-    src = fetchFromGitHub {
-      owner = "mutwo-org";
-      repo = name;
-      rev = "23331d320fa3957eeac0c12d1b664ad8c3265087";
-      sha256 = "sha256-zg0sGbKVGCr0EfgU74FrNP66ljgPB/wVA0Kf4g/aaqQ=";
-    };
-    checkInputs = [
-      python310Packages.pytest
-    ];
-    propagatedBuildInputs = [
-      mutwo-music
-    ];
-    checkPhase = ''
-      runHook preCheck
-      pytest
-      runHook postCheck
-    '';
-    doCheck = true;
-  }
+  mutwo-ekmelily-local
